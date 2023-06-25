ec54d08ac601836d7efe3d855609b2d33b51ef21
Vyshnav C J <vyshnavjenilkumar@gmail.com>
2023-06-25 18:09:52 +0530

    fix: fixed the file routing

---
+++ static/js/main.js
@@ -0,0 +1,92 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+        } else {
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        items: 1,
+        dots: false,
+        loop: true,
+        nav: true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+    
+// Select the necessary elements
+const audioFileInput = document.getElementById('audioFileInput');
+const uploadButton = document.getElementById('uploadButton');
+
+// Handle the upload button click event
+uploadButton.addEventListener('click', function() {
+  const file = audioFileInput.files[0];
+  if (file) {
+    const formData = new FormData();
+    formData.append('audioFile', file);
+
+    fetch('/process-audio', {
+      method: 'POST',
+      body: formData
+    })
+    .then(function(response) {
+      // Handle the server response
+      // ...
+    })
+    .catch(function(error) {
+      console.error('Error processing audio:', error);
+    });
+  } else {
+    console.log('No file selected.');
+  }
+});
+
+})(jQuery);
